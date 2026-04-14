e11c5c63c33b70008bba9d9318b36fc646f1c3f1
Patrick Nguyen <patricknguy789@gmail.com>
2026-04-13 21:02:25 -0700

    execute module, done

---
+++ Execute/src/execute.v
@@ -0,0 +1,113 @@
+module execute(
+    input wire clk,ALUSrc,RegDst,
+    input wire [1:0] WB,ALUOp
+    input wire [2:0] Mem,
+    input wire [31:0] NPC, ReadData1, ReadData2, SignExtend,
+    input wire [4:0] Instr_2016,Instr_1511
+    
+);
+    //connecting wires
+    wire [31:0] top_mux_out;
+    wire [2:0] alucontrol_out;
+
+    // input to latch
+    wire [31:0] adder_out, alu_out, read_dat2;
+    wire [4:0] bottom_mux_out;
+    wire alu_zero;
+    
+    ex_mem_latch u_ex_mem_latch(
+        //inputs
+        .clk             	(clk              ),
+        .ctrl_wb_in      	(WB      ),
+        .ctrl_mem_in     	(Mem      ),
+        .adder_out       	(adder_out        ),
+        .alu_zero        	(alu_zero         ),
+        .alu_out         	(alu_out          ),
+        .read_dat2       	(read_dat2        ),
+        .mux_out         	(bottom_mux_out   ),
+
+        //outputs
+        .ctrl_wb_out     	(ctrl_wb_out      ),
+        .ctrl_mem_out    	(ctrl_mem_out     ),
+        .branch          	(branch           ),
+        .memread         	(memread          ),
+        .memwrite        	(memwrite         ),
+        .add_result      	(add_result       ),
+        .zero            	(zero             ),
+        .alu_result      	(alu_result       ),
+        .read_dat2out    	(read_dat2out     ),
+        .five_bit_muxout 	(five_bit_muxout  )
+    );
+    
+    adder u_adder(
+        //input
+        .add_in1(NPC),
+        .add_in2(SignExtend),
+
+        //output
+        .add_out(adder_out)
+    );
+
+    top_mux u_top_mux(
+        //input
+        .ReadData2(ReadData2),
+        .SignExtend(SignExtend),
+        .ALUSrc(ALUSrc),
+
+        //output
+        .top_mux_out(top_mux_out)
+    );
+    
+    bottom_mux u_bottom_mux(
+        .Instr_2016     	(Instr_2016      ),
+        .Instr_1511     	(Instr_1511      ),
+        .RegDst         	(RegDst          ),
+        .bottom_mux_out 	(bottom_mux_out  )
+    );
+    
+    // output declaration of module alu
+    reg [31:0] result;
+    wire zero;
+    
+    alu #(
+        .ALUadd 	(010  ),
+        .ALUsub 	(110  ),
+        .ALUand 	(000  ),
+        .ALUor  	(001  ),
+        .ALUslt 	(111  ))
+    u_alu(
+        .rdata1    	(ReadData1  ),
+        .muxoutput 	(top_mux_out  ),
+        .sel       	(alucontrol_out   ),
+        .result    	(alu_out     ),
+        .zero      	(alu_zero     )
+    );
+    
+    // output declaration of module alucontrol
+    reg [2:0] select;
+    
+    alucontrol #(
+        .Rtype    	(10      ),
+        .lwsw     	(00      ),
+        .Itype    	(01      ),
+        .xis      	(XXXXXX  ),
+        .ALUadd   	(010     ),
+        .ALUsub   	(110     ),
+        .ALUand   	(000     ),
+        .ALUor    	(001     ),
+        .ALUslt   	(111     ),
+        .unknown  	(11      ),
+        .ALUx     	(011     ),
+        .FUNCTadd 	(100000  ),
+        .FUNCTsub 	(100010  ),
+        .FUNCTand 	(100100  ),
+        .FUNCTor  	(100101  ),
+        .FUNCTslt 	(101010  ))
+    u_alucontrol(
+        .funct  	(SignExtend[5:0]   ),
+        .aluop  	(ALUOp   ),
+        .select 	(alucontrol_out  )
+    );
+    
+
+endmodule
