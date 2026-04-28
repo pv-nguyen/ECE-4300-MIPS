26397d0b3fac8d27c21ac7c1842541e91c22bee8
Dia <agrawaldia52@gmail.com>
2026-04-27 21:36:05 -0700

    help

--- Memory_Writeback/src/data_memory.v
+++ Memory_Writeback/src/data_memory.v
@@ -9,16 +9,22 @@ module data_memory (
    reg [31:0] DMEM[0:255];  // 256 (2^8) words of 32-bit memory
    integer i;
 
-    initial begin
-            read_data <= 0;
-        //  Initialize DMEM[0-5] from data.txt
-        //  This is testing the MIPS datapath (lab 6)
-            $readmemb("../src/data.txt",DMEM);
-            for (i = 0; i < 6; i = i + 1)
-                $display("\tDMEM[%0d] = %0b", i, DMEM[i]);
-   end
+//     initial begin
+//             read_data <= 0;
+//         //  Initialize DMEM[0-5] from data.txt
+//         //  This is testing the MIPS datapath (lab 6)
+//             $readmemb("../src/data.txt",DMEM);
+//             for (i = 0; i < 6; i = i + 1)
+//                 $display("\tDMEM[%0d] = %0b", i, DMEM[i]);
+//    end
    
    always@(posedge clk) begin
+        DMEM[0] <= 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+        DMEM[1] <= 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+        DMEM[2] <= 32'b0000_0000_0000_0000_0000_0000_0000_0010;
+        DMEM[3] <= 32'b0000_0000_0000_0000_0000_0000_0000_0011;
+        DMEM[4] <= 32'b0000_0000_0000_0000_0000_0000_0000_0100;
+        DMEM[5] <= 32'b0000_0000_0000_0000_0000_0000_0000_0101;
         if (memread) begin
                 read_data <= DMEM[addr]; //grabs data at the address specified by the addr wire 
         end
--- full-pipeline/full_pipeline.v
+++ full-pipeline/full_pipeline.v
@@ -1,10 +1,34 @@
+`timescale 1ns / 1ps
+
 `include "../Instruction-Fetch/fetch.v"
+`include "../Instruction-Fetch/ifIdLatch.v"
+`include "../Instruction-Fetch/incrementer.v"
+`include "../Instruction-Fetch/instrMem.v"
+`include "../Instruction-Fetch/mux.v"
+`include "../Instruction-Fetch/pc.v"
+
 `include "../Decode/src/decode.v"
+`include "../Decode/src/control.v"
+`include "../Decode/src/idExLatch.v"
+`include "../Decode/src/registerFile.v"
+`include "../Decode/src/signExtend.v"
+
+`include "../Execute/src/execute.v"
+`include "../Execute/src/adder.v"
+`include "../Execute/src/alu.v"
+`include "../Execute/src/alucontrol.v"
+`include "../Execute/src/bottom_mux.v"
+`include "../Execute/src/ex_mem_latch.v"
+`include "../Execute/src/top_mux.v"
+
+`include "../Memory_Writeback/src/memory.v"
+`include "../Memory_Writeback/src/and.v"
+`include "../Memory_Writeback/src/data_memory.v"
+`include "../Memory_Writeback/src/memWbLatch.v"
+`include "../Memory_Writeback/src/mux.v"
 
 module full_pipeline(
-
-    input wire clk,
-                rst,
+    input wire clk, rst
 ); 
 //FETCH OUTPUT 
 wire [31:0] instr_from_IfId_latch, 
@@ -17,12 +41,12 @@ wire [3:0] id_ex_execute;
 wire [31:0] id_ex_npc,
             id_ex_readdat1,
             id_ex_readdat2,
-            id_ex_sign_ext,
+            id_ex_sign_ext;
 wire [4:0] id_ex_instr_2016,
-            id_ex_instr_1511
+            id_ex_instr_1511;
 
 //EX/MEM LATCH OUTPUT 
-wire [2:0] ex_mem_wb; 
+wire [1:0] ex_mem_wb; 
 wire MemRead,
     MemWrite, 
     MemBranch,
@@ -33,10 +57,10 @@ wire [31:0] ex_mem_readdata2;
 wire [4:0] ex_mem_muxout; 
 
 //MEM/WB LATCH OUTPUT
-wire [1:0] mem_wb_RegWrite
-wire mem_wb_PCSrc 
-wire [31:0] mem_wb_write_data 
-wire [4:0] mem_wb_write_reg
+wire mem_wb_RegWrite;
+wire mem_wb_PCSrc;
+wire [31:0] mem_wb_write_data; 
+wire [4:0] mem_wb_write_reg;
 
 fetch u_fetch (
         .clk (clk),
@@ -44,8 +68,8 @@ fetch u_fetch (
         .ex_mem_pc_src (mem_wb_PCSrc), //DONE
         .ex_mem_npc (PC_from_ExMem), 
         .if_id_instr (instr_from_IfId_latch), 
-        .if_id_npc (NPC_from_IfId_latch), 
-)
+        .if_id_npc (NPC_from_IfId_latch)
+);
 
 decode u_decode (
         .clk(clk), //DONE 
@@ -64,9 +88,9 @@ decode u_decode (
         .id_ex_sign_ext(id_ex_sign_ext), //DONE
         .id_ex_instr_bits_20_16(id_ex_instr_2016), //DONE
         .id_ex_bits_15_11(id_ex_instr_1511) //DONE
-    )
+    );
 
-execute u_decode(
+execute u_execute(
         .clk(clk), //DONE
         .WB(id_ex_wb), //DONE
         .Mem(id_ex_mem),  //DONE
@@ -103,6 +127,6 @@ memory u_memory (
         .write_data(mem_wb_write_data), //DONE
         .mem_write_reg(mem_wb_write_reg), //DONE
         .PCSrc(mem_wb_PCSrc) //DONE
-)
+);
 
 endmodule 
