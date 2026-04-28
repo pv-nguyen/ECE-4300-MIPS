8a6b7a156ce10b8fa3bf1c6cbd11989a44a8ead3
Dia <agrawaldia52@gmail.com>
2026-04-27 23:16:22 -0700

    x

--- Memory_Writeback/src/data_memory.v
+++ Memory_Writeback/src/data_memory.v
@@ -2,7 +2,7 @@
 module data_memory (
     input wire [31:0] addr, // Memory Address
     input wire [31:0] write_data, // Memory Address Contents
-    input wire memread, memwrite,clk, // refer to Lab 2-2 Figure 2.2
+    input wire memread, memwrite,clk, rst // refer to Lab 2-2 Figure 2.2
     output reg [31:0] read_data // Output of Memory Address Contents
     );
 // Register Declaration
@@ -18,13 +18,15 @@ module data_memory (
 //                 $display("\tDMEM[%0d] = %0b", i, DMEM[i]);
 //    end
    
-   always@(posedge clk) begin
-        DMEM[0] <= 32'b0000_0000_0000_0000_0000_0000_0000_0000;
-        DMEM[1] <= 32'b0000_0000_0000_0000_0000_0000_0000_0001;
-        DMEM[2] <= 32'b0000_0000_0000_0000_0000_0000_0000_0010;
-        DMEM[3] <= 32'b0000_0000_0000_0000_0000_0000_0000_0011;
-        DMEM[4] <= 32'b0000_0000_0000_0000_0000_0000_0000_0100;
-        DMEM[5] <= 32'b0000_0000_0000_0000_0000_0000_0000_0101;
+   always@(posedge clk or negedge rst) begin
+        if (rst) begin 
+            DMEM[0] <= 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+            DMEM[1] <= 32'b0000_0000_0000_0000_0000_0000_0000_0001;
+            DMEM[2] <= 32'b0000_0000_0000_0000_0000_0000_0000_0010;
+            DMEM[3] <= 32'b0000_0000_0000_0000_0000_0000_0000_0011;
+            DMEM[4] <= 32'b0000_0000_0000_0000_0000_0000_0000_0100;
+            DMEM[5] <= 32'b0000_0000_0000_0000_0000_0000_0000_0101;
+        end
         if (memread) begin
                 read_data <= DMEM[addr]; //grabs data at the address specified by the addr wire 
         end
